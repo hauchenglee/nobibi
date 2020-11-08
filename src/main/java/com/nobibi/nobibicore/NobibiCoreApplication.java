a3d7545dc5e1d2ea7084220174ceb1663a5d3cd9
hauchenglee <hauchenglee@gmail.com>
2020-11-08 22:03:29 +0800

    First commit

---
+++ src/main/java/com/nobibi/nobibicore/NobibiCoreApplication.java
@@ -0,0 +1,13 @@
+package com.nobibi.nobibicore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NobibiCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NobibiCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nobibi/nobibicore/NobibiCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobibi.nobibicore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobibiCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
